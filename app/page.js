e1a3fcac31029e663fd43070c89d245c04b74641
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-01-22 16:27:58 +0100

    removed heading section

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
          height={300}
          alt='Image Logo'
          />
-       {/*} <h2 style={{color:'white', fontSize:'20', zIndex: '11', textAlign: 'center', paddingLeft: '5%' }}>Welcome to S.P.A.C.E , Art and NFT project</h2>*/}
+      
         <section className={styles.nftGallery}>
           
           <ArtBox src='/images/1_005.png' alt='nft image' />
